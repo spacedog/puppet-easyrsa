836d3eb7abbbe0d7f254ef0b9c528aeebfaaed6e
Rehan Mahmood <rehanone@gmail.com>
2016-12-20 22:43:45 -0500

    Improved unit tests.

--- manifests/dh.pp
+++ manifests/dh.pp
@@ -9,7 +9,7 @@ define easyrsa::dh (
   $pki = "${easyrsa::pkiroot}/${title}"
 
   exec { "build-dh-${pki_name}":
-    command   => "${easyrsa::install_dir}/easyrsa --pki-dir=${pki} --keysize=${key_size} --batch gen-dh",
+    command   => "${easyrsa::install_dir}/easyrsa --pki-dir='${pki}' --keysize=${key_size} --batch gen-dh",
     cwd       => $easyrsa::install_dir,
     creates   => "${pki}/dh.pem",
     provider  => shell,
