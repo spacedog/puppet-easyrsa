900a62b06a4c7e1ff4ce4061374f70e5bb79a1e6
Rehan Mahmood <rehanone@gmail.com>
2016-12-21 21:42:05 -0500

    Acceptance tests

--- examples/init.pp
+++ examples/init.pp
@@ -1,1 +1,19 @@
-class { 'easyrsa': }
+node default {
+
+  notify { 'enduser-before': }
+  notify { 'enduser-after': }
+
+  class { 'easyrsa':
+    pkis    => {
+      'test-pki' => {},
+    },
+    cas     => {
+      'test-pki' => {},
+    },
+    dhparms => {
+      'test-pki' => {},
+    },
+    require => Notify['enduser-before'],
+    before  => Notify['enduser-after'],
+  }
+}
