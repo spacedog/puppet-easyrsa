b0390f336f1260d02558f7485bab14828859cee8
Rehan Mahmood <rehanone@gmail.com>
2019-01-03 21:22:47 -0500

    - Updated `Easy RSA` from version 3.0.3 to 3.0.5. - Updated tests.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,47 +1,82 @@
 require 'spec_helper_acceptance'
 
 describe 'easyrsa class:', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
-  it 'easyrsa is expected run successfully' do
-    pp = "class { 'easyrsa': }"
-
-    # Apply twice to ensure no errors the second time.
-    apply_manifest(pp, catch_failures: true) do |r|
-      expect(r.stderr).not_to match(%r{error}i)
+  context 'run with default settings' do
+    before(:each) do
+      shell 'rm -fv /etc/easyrsa/*'
     end
-    apply_manifest(pp, catch_failures: true) do |r|
-      expect(r.stderr).not_to eq(%r{error}i)
 
-      expect(r.exit_code).to be_zero
+    let(:manifest) do
+      <<-EOS
+        class { 'easyrsa': }
+      EOS
+    end
+
+    let(:result) { apply_manifest(manifest, catch_failures: true) }
+
+    it 'runs without errors' do
+      expect(result.exit_code).to eq 2
     end
   end
 
-  before(:each) do
-    shell 'rm -fv /etc/easyrsa/*'
-  end
+  context 'run with ca_manage => true and server client settings:' do
+    let(:manifest) do
+      <<-EOS
+         class { 'easyrsa':
+                 repo_manage => true,
+                 pkis => { 'CA' => {} },
+                 cas => { 'CA' => {} },
+                 dhparams => { 'CA' => {} },
+                 servers => { 'server' => { pki_name => 'CA' } },
+                 clients => { 'client' => { pki_name => 'CA' } }
+               }
+      EOS
+    end
 
-  context 'ca_manage => true:' do
-    it 'runs successfully' do
-      pp = "class { 'easyrsa':
-        repo_manage => true,
-        pkis => { 'CA' => {} },
-        cas => { 'CA' => {} },
-        dhparams => { 'CA' => {} },
-        servers => { 'server' => { pki_name => 'CA' } },
-        clients => { 'client' => { pki_name => 'CA' } }
-      }"
+    let(:result) { apply_manifest(manifest, catch_failures: true) }
 
-      apply_manifest(pp, catch_failures: true) do |r|
-        expect(r.stderr).not_to match(%r{error}i)
-      end
+    it 'runs without errors' do
+      expect(result.exit_code).to eq 2
+    end
 
-      shell('test -e /etc/easyrsa/CA')
-      shell('test -e /etc/easyrsa/CA/ca.crt')
-      shell('test -e /etc/easyrsa/CA/private/ca.key')
-      shell('test -e /etc/easyrsa/CA/dh.pem')
-      shell('test -e /etc/easyrsa/CA/issued/server.crt')
-      shell('test -e /etc/easyrsa/CA/private/server.key')
-      shell('test -e /etc/easyrsa/CA/issued/client.crt')
-      shell('test -e /etc/easyrsa/CA/private/client.key')
+    it 'generates CA directory' do
+      result = shell('test -e /etc/easyrsa/CA')
+      expect(result.exit_code).to eq 0
+    end
+
+    it 'generates CA certificate' do
+      result = shell('test -e /etc/easyrsa/CA/ca.crt')
+      expect(result.exit_code).to eq 0
+    end
+
+    it 'generates CA key' do
+      result = shell('test -e /etc/easyrsa/CA/private/ca.key')
+      expect(result.exit_code).to eq 0
+    end
+
+    it 'generates dh params' do
+      result = shell('test -e /etc/easyrsa/CA/dh.pem')
+      expect(result.exit_code).to eq 0
+    end
+
+    it 'generates server certificate' do
+      result = shell('test -e /etc/easyrsa/CA/issued/server.crt')
+      expect(result.exit_code).to eq 0
+    end
+
+    it 'generates server key' do
+      result = shell('test -e /etc/easyrsa/CA/private/server.key')
+      expect(result.exit_code).to eq 0
+    end
+
+    it 'generates client certificate' do
+      result = shell('test -e /etc/easyrsa/CA/issued/client.crt')
+      expect(result.exit_code).to eq 0
+    end
+
+    it 'generates client key' do
+      result = shell('test -e /etc/easyrsa/CA/private/client.key')
+      expect(result.exit_code).to eq 0
     end
   end
 end
